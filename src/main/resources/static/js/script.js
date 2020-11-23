a1f136d70edd0a4f710f224987af3951cdf77a37
Nilesh Ginoya <61002154+Nilesh7337@users.noreply.github.com>
2020-11-23 17:28:35 +0530

    search function complite

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -15,3 +15,41 @@ const toggleSidebar = () => {
     }
 };
 
+const search = () => {
+
+    let query = $("#search-input").val();
+
+
+    if (query == '') {
+        $(".search-result").hide();
+    } else {
+
+        //search 
+        console.log(query);
+
+        // sending request to server
+
+        let url = `http://localhost:8080/search/${query}`;
+
+        fetch(url).then(Response => {
+                return Response.json();
+            })
+            .then((data) => {
+                //data......
+                //console.log(data);
+
+
+                let text = `<div class='list-group'>`;
+
+                data.forEach((contact) => {
+                    text += `<a href='/user/${contact.cId}/contact' class='list-group-item list-group-action'> ${contact.name} </a>`
+                });
+
+                text += `</div>`;
+
+
+                $(".search-result").html(text);
+                $(".search-result").show();
+            });
+    }
+};
